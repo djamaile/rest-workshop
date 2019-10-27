a2065f73af691d12e0cac3866896474cefc9027c
djamaile <rdjamaile@gmail.com>
2019-10-27 18:09:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [url, setUrl] = useState("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=DEMO_KEY");
+  const [nasaData, setNasaData] = useState([]);
+  useEffect(() => {
+
+    async function demo(){
+     console.log("Working...")
+    }
+
+    demo();
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
